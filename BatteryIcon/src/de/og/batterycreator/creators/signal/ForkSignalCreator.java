0a83944e45ed9852a79b6340f0a2ea5dfb800611
olivergeith <oliver.geith@gmx.net>
2012-10-29 18:04:51 +0100

    ForkSignal optimized, löitte framework mod addet

--- BatteryIcon/src/de/og/batterycreator/creators/signal/ForkSignalCreator.java
+++ BatteryIcon/src/de/og/batterycreator/creators/signal/ForkSignalCreator.java
@@ -13,7 +13,7 @@ public class ForkSignalCreator extends AbstractSignalCreator {
 
 	private static final int imgWidth = 41;
 	private static final int imgHeight = 41;
-	private static final int gap = 2;
+	private static final int gap = 1;
 	private final int breite = Math.round((imgWidth - 2 - (4 * gap)) / 5f);
 
 	public ForkSignalCreator() {
@@ -58,6 +58,13 @@ public class ForkSignalCreator extends AbstractSignalCreator {
 				}
 			}
 		}
+
+		g2d.setColor(settings.getIconColorInActiv());
+		final Rectangle rectout = new Rectangle(0, imgHeight - 5, imgWidth / 2 - 2, 5);
+		final Rectangle rectin = new Rectangle(imgWidth / 2, imgHeight - 5, imgWidth / 2 - 1, 5);
+		g2d.fillRect(rectin.x, rectin.y, rectin.width, rectin.height);
+		g2d.fillRect(rectout.x, rectout.y, rectout.width, rectout.height);
+
 		// Filewriting
 		img = writeFile(getFileName(level, fully), img);
 		return new ImageIcon(img);
@@ -69,8 +76,8 @@ public class ForkSignalCreator extends AbstractSignalCreator {
 		BufferedImage img = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_ARGB);
 		final Graphics2D g2d = initGrafics2D(img, true);
 
-		final Rectangle rectout = new Rectangle(0, imgHeight - 6, imgWidth / 2 - 2, 5);
-		final Rectangle rectin = new Rectangle(imgWidth / 2 + 2, imgHeight - 6, imgWidth / 2 - 2, 5);
+		final Rectangle rectout = new Rectangle(0, imgHeight - 5, imgWidth / 2 - 2, 5);
+		final Rectangle rectin = new Rectangle(imgWidth / 2, imgHeight - 5, imgWidth / 2 - 1, 5);
 
 		// final Rectangle rectout = new Rectangle(0, imgHeight - 6, imgWidth,
 		// 3);

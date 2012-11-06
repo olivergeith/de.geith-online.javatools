c5bd9a4c32741046115aaff0afe62f632179e243
olivergeith <oliver.geith@gmx.net>
2012-11-06 20:15:48 +0100

    RecurseFileSet first implemantation

--- BatteryIcon/src/de/og/batterycreator/main/IconCreatorFrame.java
+++ BatteryIcon/src/de/og/batterycreator/main/IconCreatorFrame.java
@@ -18,8 +18,8 @@ import de.og.batterycreator.gui.iconstore.IconStore;
 public class IconCreatorFrame extends JFrame {
 
 	private static final String APP_NAME = "The Battery Icon Creator";
-	public static final String VERSION_NR = "12.0";
-	private static final String VERSION_DATE = "05.11.2012";
+	public static final String VERSION_NR = "14.0";
+	private static final String VERSION_DATE = "xx.11.2012";
 	private static final long serialVersionUID = 1L;
 	private static IconCreatorFrame frame;
 	private BeendenAktion beendenAktion;
--- BatteryIcon/src/de/og/batterycreator/zipcreator/ZipElementCollection.java
+++ BatteryIcon/src/de/og/batterycreator/zipcreator/ZipElementCollection.java
@@ -9,11 +9,34 @@ public class ZipElementCollection {
 	public ZipElementCollection() {
 	}
 
+	/**
+	 * Adds files to the collection, that will end all in one path
+	 * 
+	 * @param elements
+	 * @param pathInArchiv
+	 */
 	public void addElements(final Vector<String> elements, final String pathInArchiv) {
 
 		if (elements != null && pathInArchiv != null && pathInArchiv.length() > 0) {
 			for (final String ele : elements) {
 				zipelEments.add(new ZipElement(ele, pathInArchiv));
+			}
+		}
+	}
+
+	/**
+	 * Adds files to the collection all with an indevidual path
+	 * 
+	 * @param elements
+	 * @param pathInArchiv
+	 */
+	public void addElements(final Vector<String> elements, final Vector<String> pathInArchiv) {
+
+		if (elements != null && pathInArchiv != null) {
+			if (elements.size() == pathInArchiv.size()) {
+				for (int i = 0; i < elements.size(); i++) {
+					zipelEments.add(new ZipElement(elements.get(i), pathInArchiv.get(i)));
+				}
 			}
 		}
 	}

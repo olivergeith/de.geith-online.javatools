ad185db86650face256bf23a7539b22cabe2167e
olivergeith <oliver.geith@gmx.net>
2012-11-21 21:59:12 +0100

    Full Circle Animation

--- SaxBReader/src/de/og/orgdatenreader/BattAnimater.java
+++ SaxBReader/src/de/og/orgdatenreader/BattAnimater.java
@@ -23,7 +23,8 @@ public class BattAnimater {
 	public static void main(final String[] args) throws Exception {
 		// Package
 		// readAnimation();
-		writeAnimation();
+		writeFullAnimation();
+		writeJKayAnimation();
 	}
 
 	public static void readAnimation() throws JAXBException {
@@ -44,7 +45,7 @@ public class BattAnimater {
 		}
 	}
 
-	public static void writeAnimation() throws JAXBException {
+	public static void writeFullAnimation() throws JAXBException {
 
 		final LevelList levelList = new LevelList();
 		final List<Item> levelItems = levelList.getItem();
@@ -87,7 +88,55 @@ public class BattAnimater {
 		final JAXBContext jc = JAXBContext.newInstance("de.og.battanimation");
 		final Marshaller m = jc.createMarshaller();
 		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		m.marshal(levelList, new File("./android/stat_sys_battery_charge_circle.xml"));
+		m.marshal(levelList, new File("./android/stat_sys_battery_charge_circle(full).xml"));
 
 	}
+
+	public static void writeJKayAnimation() throws JAXBException {
+
+		final LevelList levelList = new LevelList();
+		final List<Item> levelItems = levelList.getItem();
+
+		for (int i = 0; i <= 101; i++) {
+			// Creating LevelIem
+			final Item it = new Item();
+			it.setMaxLevel(i);
+
+			// setting animationlist
+			final AnimationList anim = new AnimationList();
+			anim.setOneshot(false);
+			it.setAnimationList(anim);
+
+			// creating drawable Items
+			final List<Item> drawableItems = anim.getItem();
+
+			// setting fist animation png
+			final Item mainit = new Item();
+			mainit.setDuration(DURATION);
+			if (i == 101) {
+				mainit.setDrawable("@drawable/stat_sys_battery_circle_full");
+			} else {
+				mainit.setDrawable("@drawable/stat_sys_battery_circle_" + i);
+			}
+			drawableItems.add(mainit);
+
+			// adding the charge png's
+			for (int j = i + 1; j <= 100; j++) {
+				final Item anit = new Item();
+				anit.setDuration(DURATION_ANIMATION);
+				anit.setDrawable("@drawable/stat_sys_battery_circle_charge_anim" + j);
+				drawableItems.add(anit);
+			}
+
+			// adding the level item
+			levelItems.add(it);
+		}
+
+		final JAXBContext jc = JAXBContext.newInstance("de.og.battanimation");
+		final Marshaller m = jc.createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		m.marshal(levelList, new File("./android/stat_sys_battery_charge_circle(jkay).xml"));
+
+	}
+
 }

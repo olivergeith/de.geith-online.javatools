c9d652923849034938410d57580a049c5f0edba1
olivergeith <oliver.geith@gmx.net>
2013-01-18 10:04:03 +0100

    Jbfs entfernt

--- FlashGeith2/gen/de/geithonline/flashgeith2/R.java
+++ FlashGeith2/gen/de/geithonline/flashgeith2/R.java
@@ -15,8 +15,7 @@ public final class R {
         public static final int button_on=0x7f020001;
         public static final int icon=0x7f020002;
         public static final int logo=0x7f020003;
-        public static final int pspbrwse=0x7f020004;
-        public static final int wallpaper2=0x7f020005;
+        public static final int wallpaper2=0x7f020004;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
--- PeugeotHaase/gen/de/geithonline/peugeothaase/R.java
+++ PeugeotHaase/gen/de/geithonline/peugeothaase/R.java
@@ -12,8 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int pspbrwse=0x7f020001;
-        public static final int wallpaper=0x7f020002;
+        public static final int wallpaper=0x7f020001;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f060000;

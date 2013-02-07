3c8d849c0993f2ad3dd334ca7dfc41d0886821db
olivergeith <oliver.geith@gmx.net>
2013-02-07 17:57:32 +0100

    Preparing V19

--- BatteryIcon/src/de/og/batterycreator/main/IconCreatorFrame.java
+++ BatteryIcon/src/de/og/batterycreator/main/IconCreatorFrame.java
@@ -25,7 +25,7 @@ public class IconCreatorFrame extends JFrame {
 	private final JButton exitButton = new JButton(CommonIconProvider.BUTTON_ICON_EXIT);
 	private static final String APP_NAME = "'The Rom Fumbler'";
 	public static final String VERSION_NR = "19.0";
-	private static final String VERSION_DATE = "xx.Feb.2013";
+	private static final String VERSION_DATE = "08.Feb.2013";
 	private static final long serialVersionUID = 1L;
 	private static IconCreatorFrame frame;
 	private final IconCreatingPanelNew iconCreatingPanel = new IconCreatingPanelNew(this);
